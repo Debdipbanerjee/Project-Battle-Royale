946013c0330dc17cbf96cebc93997da50930e209
Debdipbanerjee <debdipbanerjee7@gmail.com>
2021-05-30 23:04:38 +0530

    Looking Left & Right implemented

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform viewPoint;
+    public float mouseSensitivity = 1f;
+    private float verticalRotStore; //limit looking up & down
+    private Vector2 mouseInput;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        mouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y")) * mouseSensitivity;
+
+        //Change the rotation accoss the Y axis (looking left & right)
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, transform.rotation.eulerAngles.y + mouseInput.x, transform.rotation.eulerAngles.z);
     }
 }
