5ebbb30c25bbda445155fd450c7731d70f15f955
Debdipbanerjee <debdipbanerjee7@gmail.com>
2021-06-10 18:24:14 +0530

    Fire Impact added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -26,6 +26,8 @@ public class PlayerController : MonoBehaviour
     private bool isGrounded;
     public LayerMask groundLayers;
 
+    public GameObject bulletImpact;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -131,6 +133,10 @@ public class PlayerController : MonoBehaviour
         if(Physics.Raycast(ray,out RaycastHit hit))
         {
             Debug.Log("You hit " + hit.collider.gameObject.name);
+
+            GameObject bulletImpactObject = Instantiate(bulletImpact, hit.point + (hit.normal * 0.002f), Quaternion.LookRotation(hit.normal, Vector3.up));
+
+            Destroy(bulletImpactObject, 10f);
         }
 
     }
